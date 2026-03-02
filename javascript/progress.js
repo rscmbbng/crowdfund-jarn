09669c35c23663e397ec11590e1954921ca75511
louilinn <linne.louise@gmail.com>
2026-03-02 12:10:10 +0100

    Fix automatic missing funds number

--- javascript/progress.js
+++ javascript/progress.js
@@ -1,8 +1,15 @@
+let total = 1600000;
+let remainder = 1600000;
+
 $('.value').each(function() {
-	var total = 1600000;
-	var text = $(this).text().replace(/\s+/g, '');
-	var percentage = text/total*100+'%';
+	let textNumber = $(this).text().replace(/\s+/g, '');
+	let percentage = textNumber/total*100+'%';
+	remainder = remainder - textNumber;
 	$(this).parent().css('width', percentage);
 });
+
+const rest = document.getElementById('saknas');
+rest.innerHTML = `<span class="value">${remainder}</span>`;
+$(rest).css('width', remainder/total*100+'%');
 
 //$('.block').tooltip();
